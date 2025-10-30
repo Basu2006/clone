8c8b787022e24361a6858491a1637cb66fc2c945
Basu2006 <basavarajholal26@gmail.com>
2025-10-30 15:47:14 +0530

    Create student.py

---
+++ student.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+address = input("Enter your address: ")
+usn = input("Enter your USN: ")
+
+print(f"Name: {name}")
+print(f"Address: {address}")
+print(f"USN: {usn}")
